3af780bc6d31daec2305307b2a1c8f3be8ba1c56
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2014-09-15 18:49:06 +0530

    [ADD] Added EduERP v8 modules

---
+++ school_transport/__openerp__.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2011-2012 Serpent Consulting Services (<http://www.serpentcs.com>)
+#    Copyright (C) 2013-2014 Serpent Consulting Services (<http://www.serpentcs.com>)
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "Transport Management",
+    "version" : "2.0",
+    "author" : "Serpent Consulting Services Pvt. Ltd.",
+    "website" : "http://www.serpentcs.com",
+    "category": "School Management",
+    "complexity": "easy",
+    "description": """A module to School Transport Management.
+    A Module support the following functionalities:
+    
+    1. In This Module we can define the Transport Root and Points with vehicles.
+    2. student can do Registration for any root with selected point for specific month.
+    3. Vehicle information.
+    4. Driver information.
+    
+    """,
+    "depends" : [
+        "hr",
+        "school"
+    ],
+    "demo" : [
+              "demo/transport_demo.xml"
+              ],
+    "data" : [
+        "transport_view.xml",
+        "report/participant_report.xml",
+        "report/vehicle_report.xml",
+        "wizard/transfer_vehicle.xml",
+        "security/ir.model.access.csv",
+    ],
+    
+    'test': [
+        "test/transport.yml",
+        "test/transport_report.yml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ school_transport/report/vehicle.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2011-2012 Serpent Consulting Services (<http://www.serpentcs.com>)
+#    Copyright (C) 2013-2014 Serpent Consulting Services (<http://www.serpentcs.com>)
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from openerp.report import report_sxw
+
+class vehicle(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context=None):
+        super(vehicle, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+        })
+
+report_sxw.report_sxw('report.transport.vehicle', 'transport.vehicle', 'addons/school_transport/report/vehicle.rml', parser=vehicle, header="external")
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
