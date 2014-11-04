44138308d829b9801e0197cb0d493a4b4b704fd8
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2014-11-04 22:46:35 +0530

    fix : got rid of mx dependency

--- library/sale.py
+++ library/sale.py
@@ -20,7 +20,8 @@
 #
 ##############################################################################
 from openerp.osv import osv,fields
-from mx import DateTime
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from openerp import workflow
 
 class sale_order_line(osv.Model):
@@ -113,7 +114,7 @@ class sale_order(osv.Model):
             picking_id = False
             for line in order.order_line:
                 proc_id = False
-                date_planned = (DateTime.now() + DateTime.RelativeDateTime(days=line.delay or 0.0)).strftime('%Y-%m-%d')
+                date_planned = (datetime.now() + relativedelta(days=line.delay or 0.0)).strftime('%Y-%m-%d')
                 if line.state == 'done':
                     continue
                 if line.product_id and line.product_id.product_tmpl_id.type in ('product', 'consu'):
