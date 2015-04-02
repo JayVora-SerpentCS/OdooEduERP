c7e90fbb222b505633792f994ba5bd49b02c6726
pankaj <goyani@gmail.com>
2015-04-02 13:25:32 +0530

    [ADD/IMP] school_transport: Added qweb report in school transport module.

--- school_transport/__init__.py
+++ school_transport/__init__.py
@@ -24,7 +24,6 @@
 # A Module to Schoool Transport Management
 #----------------------------------------------------------
 import transport
-import report
 import wizard
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
