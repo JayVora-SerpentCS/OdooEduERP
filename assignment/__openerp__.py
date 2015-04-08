290558211b8df987da8d125bcc7aab7267864f55
Hasmukh Bhalodiya <h.bhalodiya.serpentcs@gmail.com>
2015-04-08 17:39:46 +0530

    [IMP] assignment: Improved changed log.

--- assignment/__openerp__.py
+++ assignment/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     "name" : "Assignment Management",
-    "version" : "2.0",
+    "version" : "3.0",
     "author" : "Serpent Consulting Services Pvt. Ltd.",
     "website" : "http://www.serpentcs.com",
     "category": "School Management",
