c184b64af7ca3fa2d52746ae274cf28f07078f89
punit chaudhary <punit.chaudhary.serpentcs@gmail.com>
2015-07-27 18:52:28 +0530

    optimize the code as per the Travis ci Build.

--- school_hostel/hostel.py
+++ school_hostel/hostel.py
@@ -3,8 +3,10 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2011-2012 Serpent Consulting Services (<http://www.serpentcs.com>)
-#    Copyright (C) 2013-2014 Serpent Consulting Services (<http://www.serpentcs.com>)
+#    Copyright (C) 2011-2012 Serpent Consulting Services
+#    (<http://www.serpentcs.com>)
+#    Copyright (C) 2013-2014 Serpent Consulting Services
+#    (<http://www.serpentcs.com>)
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
@@ -19,26 +21,30 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-#from openerp.osv import fields, osv
-#from openerp.tools.translate import _
+# from openerp.osv import fields, osv
+# from openerp.tools.translate import _
 
 from openerp import models, fields, api, _
-from openerp.exceptions import except_orm, Warning, RedirectWarning
+from openerp.exceptions import except_orm
+
 
 class hostel_type(models.Model):
-    
+
     _name = 'hostel.type'
-    
+
     name = fields.Char('Hostel Name', required=True)
-    type = fields.Selection([('boys','Boys'),('girls','Girls'),('common','Common')], 'Hostel Type', required=True,default='common')
+    type = fields.Selection([('boys', 'Boys'), ('girls', 'Girls'),
+                             ('common', 'Common')], 'Hostel Type',
+                            required=True,default='common')
     other_info = fields.Text('Other Information')
     rector = fields.Many2one('res.partner', 'Rector')
-    room_ids =  fields.One2many('hostel.room', 'name', 'Room')
+    room_ids = fields.One2many('hostel.room', 'name', 'Room')
+
 
 class hostel_room(models.Model):
-    
+
     _name = 'hostel.room'
-    
+
     @api.one
     @api.depends('student_ids')
     def _check_availability(self):
@@ -49,7 +55,8 @@ class hostel_room(models.Model):
                 count += 1
             room_availability = data.student_per_room - count
             if room_availability < 0:
-                raise except_orm(_("You can not assign room more than %s student" % data.student_per_room))
+                raise except_orm(_("You can not assign room more"/ 
+                                   "than %s student" % data.student_per_room))
             else:
                 data.availability = room_availability
 
@@ -57,8 +64,10 @@ class hostel_room(models.Model):
     floor_no = fields.Integer('Floor No.',default=1)
     room_no = fields.Char('Room No.',required=True)
     student_per_room = fields.Integer('Student Per Room', required=True)
-    availability = fields.Float(compute='_check_availability',string="Availability")
-    student_ids = fields.One2many('hostel.student','hostel_room_id', 'Student')
+    availability = fields.Float(compute='_check_availability',
+                                string="Availability")
+    student_ids = fields.One2many('hostel.student','hostel_room_id',
+                                  'Student')
     telephone = fields.Boolean('Telephone access')
     ac = fields.Boolean('Air Conditioning')
     private_bathroom = fields.Boolean ('Private Bathroom')
@@ -67,14 +76,21 @@ class hostel_room(models.Model):
     internet = fields.Boolean ('Internet Access')
     refrigerator = fields.Boolean ('Refrigerator')
     microwave = fields.Boolean ('Microwave')
-    
 
-    _sql_constraints = [('room_no_unique', 'unique(room_no)', 'Room number must be unique!')]
-    _sql_constraints = [('floor_per_hostel','check(floor_no < 10)','Error ! Floor per hostel should be less than 10.')]
-    _sql_constraints = [('student_per_room_greater','check(student_per_room < 10)','Error ! Student per room should be less than 10.')]
+
+    _sql_constraints = [('room_no_unique',
+                         'unique(room_no)',
+                         'Room number must be unique!')]
+    _sql_constraints = [('floor_per_hostel',
+                         'check(floor_no < 10)',
+                         'Error ! Floor per hostel should be less than 10.')]
+    _sql_constraints = [('student_per_room_greater',
+                         'check(student_per_room < 10)',
+                         'Error ! Student per room should be less than 10.')]
+
 
 class hostel_student(models.Model):
-    
+
     _name = 'hostel.student'
 
     @api.one
@@ -85,7 +101,7 @@ class hostel_student(models.Model):
                 get_fee.remaining_amount = get_fee.room_rent - get_fee.paid_amount
             else:
                 get_fee.remaining_amount = 0.0
-        
+
 
     @api.multi
     def print_fee_receipt(self):
@@ -93,12 +109,16 @@ class hostel_student(models.Model):
         datas = {
             'ids': [data['id']],
             'form': data,
-            'model':'hostel.student',
+            'model': 'hostel.student',
         }
-        return {'type': 'ir.actions.report.xml', 'report_name': 'school_hostel.hostel_fee_reciept', 'datas': datas}
+        return {'type': 'ir.actions.report.xml',
+                'report_name': 'school_hostel.hostel_fee_reciept',
+                'datas': datas}
 
     hostel_room_id = fields.Many2one('hostel.room', 'Hostel Room')
-    hostel_id = fields.Char('Hostel ID',readonly=True,default=lambda obj:obj.env['ir.sequence'].get('hostel.student'))
+    hostel_id = fields.Char('Hostel ID', readonly=True,
+                            default=lambda obj:obj.env['ir.sequence'].
+                            get('hostel.student'))
     student_id = fields.Many2one('student.student', 'Student')
     school_id = fields.Many2one('school.school', 'School')
     room_rent = fields.Float('Total Room Rent', required=True)
@@ -106,26 +126,33 @@ class hostel_student(models.Model):
     admission_date = fields.Datetime('Admission Date')
     discharge_date = fields.Datetime('Discharge Date')
     paid_amount = fields.Float('Paid Amount')
-    remaining_amount = fields.Float(compute='_get_remaining_fee_amt', string='Remaining Amount')
-    status = fields.Selection([('draft','Draft'),('reservation','Reservation'),('confirm','Confirm')], 'Status',default='draft')
+    remaining_amount = fields.Float(compute='_get_remaining_fee_amt',
+                                    string='Remaining Amount')
+    status = fields.Selection([('draft', 'Draft'),
+                               ('reservation', 'Reservation'),
+                               ('confirm', 'Confirm')],
+                              'Status', default='draft')
     
-    _sql_constraints = [('admission_date_greater','check(discharge_date >= admission_date)','Error ! Discharge Date cannot be set before Admission Date.')]
+    _sql_constraints = [('admission_date_greater',
+                         'check(discharge_date >= admission_date)',
+                         'Error ! Discharge Date cannot be set before'\
+                         'Admission Date.')]
 
     @api.multi
     def confirm_state(self):
         """
-        This method is used to change the state 
+        This method is used to change the state
         to confirmed of the school hostel
         --------------------------------------------
         @param self : object pointer
         """
         for on in self:
             on.status = 'confirm'
-        
+
     @api.multi
     def reservation_state(self):
         """
-        This method is used to change the state 
+        This method is used to change the state
         to confirmed of the school hostel
         --------------------------------------------
         @param self : object pointer
@@ -133,11 +160,12 @@ class hostel_student(models.Model):
         for on in self:
             on.status = 'reservation'
 
+
 class bed_type(models.Model):
-    
+
     _name = 'bed.type'
     _description='Type of Bed in Hostel'
-        
+
     name = fields.Char('Name',required=True)
     description = fields.Text('Description')
 
