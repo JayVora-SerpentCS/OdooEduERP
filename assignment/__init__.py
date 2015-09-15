0d7034a31aa10a5c614465e747bc236c9283079b
punit chaudhary <punit.chaudhary.serpentcs@gmail.com>
2015-09-15 12:16:17 +0530

    optimize the code as per the Travis ci Build.

--- assignment/__init__.py
+++ assignment/__init__.py
@@ -21,7 +21,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import homework
+import homework
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
