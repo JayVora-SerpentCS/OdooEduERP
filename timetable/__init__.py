254aa97ca0add229238ec4b23e6d5cfa47c18268
punit chaudhary <punit.chaudhary.serpentcs@gmail.com>
2015-09-15 14:35:16 +0530

    optimize the code as per the Travis ci

--- timetable/__init__.py
+++ timetable/__init__.py
@@ -29,7 +29,7 @@
 # ----------------------------------------------------------
 from . import report
 
-from . import timetable
+from . import models
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
