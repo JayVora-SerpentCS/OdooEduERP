2d78c46aa0ad445fa1c0a7af315f6481d60466da
serpentcs <support@serpentcs.com>
2015-10-20 17:35:21 +0530

    [ADD/IMP] school_attendance : Migrated module as per V9 standard.

--- school_attendance/__openerp__.py
+++ school_attendance/__openerp__.py
@@ -1,54 +1,46 @@
-# -*- encoding: utf-8 -*-
+# -*- encoding: UTF-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2011-2012 Serpent Consulting Services (<http://www.serpentcs.com>)
-#    Copyright (C) 2013-2014 Serpent Consulting Services (<http://www.serpentcs.com>)
+#    Copyright (C) 2012-Today Serpent Consulting Services PVT. LTD.
+#    (<http://www.serpentcs.com>)
+#
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 ##############################################################################
 
 {
-    "name" : "Attendance Management",
-    "version" : "3.0",
-    "author" : "Serpent Consulting Services Pvt. Ltd.",
-    "website" : "http://www.serpentcs.com",
-    "category": "School Management",
-    "complexity": "easy",
-    "description": """A module to Student Attendance System.
+    'name': 'Attendance Management',
+    'version': '3.0',
+    'author': 'Serpent Consulting Services PVT. LTD.',
+    'website': 'http://www.serpentcs.com',
+    'category': 'School Management',
+    'complexity': 'easy',
+    'description': '''A module to Student Attendance System.
         A Module support the following functionalities:
             1. Class-wise Daily Attendance Sheet.
-            2. Class-wise Monthly Attendance Sheet.
-                    """,
-    "depends" : [
-        "school","hr"
+            2. Class-wise Monthly Attendance Sheet.''',
+    'depends': ['school', 'hr'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/school_attendance_view.xml',
+        'views/attendance_workflow.xml',
+        'wizard/attendance_sheet_wizard_view.xml',
+        'wizard/student_attendance_by_month_view.xml',
+#         'data/daily_attendance_data.xml',
     ],
-    "data" : [
-        "security/ir.model.access.csv",
-        "school_attendance_view.xml",
-        "attendance_workflow.xml",
-        "wizard/attendance_sheet_wizard_view.xml",
-        "wizard/student_attendance_by_month_view.xml",
-    ],
-    'demo': [
-             "demo/school_attendance_demo.xml"
-    ],
-    
-    "installable": True,
-    "auto_install": False,
-    "application": True,
+    'demo': ['demo/school_attendance_demo.xml'],
+    'installable': True,
+    'application': True,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
