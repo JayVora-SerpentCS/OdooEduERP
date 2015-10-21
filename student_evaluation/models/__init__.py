3b54b6b726152d9ad032b9ef387cfe8d6ddecc5a
serpentcs <support@serpentcs.com>
2015-10-21 19:15:36 +0530

    [IMP] student_evaluation : Migrated module as per V9 standard.

--- student_evaluation/models/__init__.py
+++ student_evaluation/models/__init__.py
@@ -20,26 +20,4 @@
 #
 ##############################################################################
 
-{
-    'name': 'Evaluation Management',
-    'version': '2.0',
-    'author': 'Serpent Consulting Services PVT. LTD.',
-    'website': 'http://www.almisnadtechnology.com',
-    'category': 'School Management',
-    'complexity': 'easy',
-    'description': ''''A module to Student Evaluation.
-    A Module support the following functionalities:
-
-    1. In This Module we can define the terms which we want to consider In Evaluation.
-    2. student and Faculty Both can Evaluate This.
-    ''',
-    'depends': ['hr', 'school'],
-    'data': ['views/student_evaluation_view.xml',
-             'security/ir.model.access.csv',
-     ],
-    'demo': [
-             'demo/student_evaluation_demo.xml'
-             ],
-    'installable': True,
-    'application': True,
-}
+from . import student_evaluation
