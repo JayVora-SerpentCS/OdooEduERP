d6600e745981f3517057be44fafe00010c8433e8
serpentcs <support@serpentcs.com>
2015-10-23 14:28:00 +0530

    [IMP] assignment : Migrated module as per V9 standard.

--- assignment/__openerp__.py
+++ assignment/__openerp__.py
@@ -1,44 +1,45 @@
-# -*- encoding: utf-8 -*-
+# -*- encoding: UTF-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2011-TODAY Serpent Consulting Services Pvt. Ltd.(<http://www.serpentcs.com>)
+#    Copyright (C) 2012-Today Serpent Consulting Services PVT. LTD.
+#    (<http://www.serpentcs.com>)
+#
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 ##############################################################################
 
 {
-    "name" : "Assignment Management",
-    "version" : "3.0",
-    "author" : "Serpent Consulting Services Pvt. Ltd.",
-    "website" : "http://www.serpentcs.com",
-    "category": "School Management",
-    "complexity": "easy",
-    "description": "A module to School Assignment Management. which help in the school management to assign the assignment to the student",
-    "depends" : ["school"],
-    "data" : [
-            "homework_view.xml",
-            "security/ir.model.access.csv",
+    'name': 'Assignment Management',
+    'version': '3.0',
+    'author': 'Serpent Consulting Services PVT. LTD.',
+    'website': 'http://www.serpentcs.com',
+    'category': 'School Management',
+    'complexity': 'easy',
+    'description': '''A module to School Assignment Management.
+                    which helps in school management to assign the assignment
+                    to the student''',
+    'depends': ['school'],
+    'data': [
+            'homework_view.xml',
+            'security/ir.model.access.csv',
         ],
-#     'demo': [
-#             "demo/assignment_demo.xml"
-#              ],
-    "test" : ['test/assignment.yml'],
+    'demo': [
+#             'demo/assignment_demo.xml'
+            ],
+    'test': ['test/assignment.yml'],
 
-    "installable": True,
-    'auto_install': False,
+    'installable': True,
+    'application': True,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
