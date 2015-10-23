64efc333ef7adfaf2113e8592e5aea79ce800271
serpentcs <support@serpentcs.com>
2015-10-23 18:05:34 +0530

    [IMP] exam : Migrated module as per V9 standard.

--- exam/report/add_exam_result.py
+++ exam/report/add_exam_result.py
@@ -1,57 +1,61 @@
-# -*- encoding: utf-8 -*-
+# -*- encoding: UTF-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2011-2012 Serpent Consulting Services (<http://www.serpentcs.com>)
-#    Copyright (C) 2013-2014 Serpent Consulting Services (<http://www.serpentcs.com>)
+#    Copyright (C) 2012-Today Serpent Consulting Services PVT. LTD.
+#    (<http://www.serpentcs.com>)
+#
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 ##############################################################################
+
 import time
 from openerp.report import report_sxw
 from openerp.osv import osv
+
 
 class add_exam_result(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context=None):
         super(add_exam_result, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
-            'get_result_detail':self._get_result_detail,
+            'get_result_detail': self._get_result_detail,
         })
 
     def _get_result_detail(self, subject_ids, result):
         sub_list = []
+        result_data = []
         for sub in subject_ids:
             sub_list.append(sub.id)
         sub_obj = self.pool.get('exam.subject')
-        subject_exam_ids = sub_obj.search(self.cr, self.uid, [('id','in',sub_list),
-                                                              ('exam_id','=',result.id)])
-        result_data = []
+        subject_exam_ids = sub_obj.search(self.cr, self.uid,
+                                          [('id', 'in', sub_list),
+                                           ('exam_id', '=', result.id)])
         for subject in sub_obj.browse(self.cr, self.uid, subject_exam_ids):
                 result_data.append({
-                    'subject':subject.subject_id and subject.subject_id.name or '',
-                    'max_mark':subject.maximum_marks or '',
-                    'mini_marks':subject.minimum_marks or '',
-                    'obt_marks':subject.obtain_marks or '',
+                    'subject': subject.subject_id
+                               and subject.subject_id.name or '',
+                    'max_mark': subject.maximum_marks or '',
+                    'mini_marks': subject.minimum_marks or '',
+                    'obt_marks': subject.obtain_marks or '',
                  })
         return result_data
 
+
 class report_add_exam_result(osv.AbstractModel):
+
     _name = 'report.exam.exam_result_report'
     _inherit = 'report.abstract_report'
     _template = 'exam.exam_result_report'
-    _wrapped_report_class = add_exam_result    
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    _wrapped_report_class = add_exam_result
