2ca0d74237a60c2261d857c7c926255b43743b15
serpentcs <support@serpentcs.com>
2015-10-23 18:05:34 +0530

    [IMP] exam : Migrated module as per V9 standard.

--- exam/models/__init__.py
+++ exam/models/__init__.py
@@ -20,6 +20,4 @@
 #
 ##############################################################################
 
-from . import exam_class_result
-from . import exam_create_result
-from . import subject_result
+from . import exam
