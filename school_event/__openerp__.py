25940a35476b0540cf0d2d46effc2a5b0e405dff
serpentcs <support@serpentcs.com>
2015-10-31 15:22:32 +0530

    [IMP] EduERPv8 : Improved code in School Attendance, School Event & School Fees Modules.

--- school_event/__openerp__.py
+++ school_event/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- encoding: UTF-8 -*-
-##############################################################################
+# -----------------------------------------------------------------------------
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2012-Today Serpent Consulting Services PVT. LTD.
@@ -18,15 +18,16 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
-##############################################################################
+# -----------------------------------------------------------------------------
 
 {
-    'name': 'School_Event',
+    'name': 'School Event Management',
     'version': '3.0',
     'author': 'Serpent Consulting Services PVT. LTD.',
     'website': 'http://www.serpentcs.com',
     'category': 'School Management',
     'complexity': 'easy',
+    'summary': 'A Module For Event Management In School',
     'description': '''A module to Event Management in School.
     A Module support the following functionalities:
     1. In This Module we can define the Event for specific class
