e4dd680fe2f81e4f15616e0030cd0a663e88c508
serpentcs <support@serpentcs.com>
2015-10-31 10:13:48 +0530

    [IMP] timetable : Improved code in School Timetable Module.

--- timetable/report/timetable_info.py
+++ timetable/report/timetable_info.py
@@ -35,18 +35,27 @@ class timetable(report_sxw.rml_parse):
 
     def _get_timetable(self, timetable_id):
         timetable_detail = []
-        self.cr.execute("select t.start_time,t.end_time,s.name,week_day,r.name as teacher from time_table_line t, subject_subject s, resource_resource r, hr_employee hr where t.subject_id= s.id and t.teacher_id= hr.id  and hr.resource_id = r.id  and table_id = %d group by start_time,end_time,s.name,week_day,r.name order by start_time" % (timetable_id.id))
+        self.cr.execute("select t.start_time,t.end_time,s.name,week_day,r.name\
+                         as teacher from time_table_line t, subject_subject s,\
+                         resource_resource r, hr_employee hr where\
+                         t.subject_id= s.id and t.teacher_id= hr.id\
+                         and hr.resource_id = r.id  and table_id = %d\
+                         group by start_time,end_time,s.name,week_day,r.name\
+                         order by start_time" % (timetable_id.id))
         res = self.cr.dictfetchall()
-#        self.cr.execute("select start_time,end_time from time_table_line where table_id=%d group by start_time,end_time  order by start_time"%(timetable_id.id))
-        self.cr.execute("select start_time,end_time from time_table_line where table_id=%d group by start_time,end_time  order by start_time" % (timetable_id.id))
+        self.cr.execute("select start_time,end_time from time_table_line\
+                         where table_id=%d group by start_time,end_time\
+                         order by start_time" % (timetable_id.id))
         time_data = self.cr.dictfetchall()
         for time_detail in time_data:
             for data in res:
-                if time_detail['start_time'] == data['start_time'] and time_detail['end_time'] == data['end_time']:
+                if time_detail['start_time'] == data['start_time']\
+                    and time_detail['end_time'] == data['end_time']:
                     if (data['name'] == 'Recess'):
                         time_detail[data['week_day']] = data['name']
                     else:
-                        time_detail[data['week_day']] = data['name'] + '\n(' + data['teacher'] + ')'
+                        time_detail[data['week_day']] = data['name']\
+                        + '\n(' + data['teacher'] + ')'
             timetable_detail.append(time_detail)
         return timetable_detail
 
