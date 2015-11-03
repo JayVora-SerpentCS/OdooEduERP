226df07a6097fdb5362872aa45fd10ee9fa6c237
serpentcs <support@serpentcs.com>
2015-11-03 12:32:22 +0530

    [IMP] EduERPv8 : Improved code in EduERP Modules.

--- school_event/__init__.py
+++ school_event/__init__.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 # ----------------------------------------------------------
 # A Module to For Management of Events in School
 # ----------------------------------------------------------
 
-from . import models
+import models
