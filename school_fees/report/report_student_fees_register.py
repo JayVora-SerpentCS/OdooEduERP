0aa5b7016482580f8c60cd85e60d938d6ed4dc34
serpentcs <support@serpentcs.com>
2015-11-03 12:32:22 +0530

    [IMP] EduERPv8 : Improved code in EduERP Modules.

--- school_fees/report/report_student_fees_register.py
+++ school_fees/report/report_student_fees_register.py
@@ -3,12 +3,13 @@
 
 import time
 from datetime import datetime
-from openerp.osv import osv
+from openerp import models, api
 from openerp.report import report_sxw
 
 
 class StudentFeesRegister(report_sxw.rml_parse):
 
+    @api.v7
     def __init__(self, cr, uid, name, context):
         super(StudentFeesRegister, self).__init__(cr, uid, name, context)
 
@@ -19,17 +20,19 @@ class StudentFeesRegister(report_sxw.rml_parse):
         })
         self.no = 0
 
+    @api.v7
     def get_no(self):
         self.no += 1
         return self.no
 
+    @api.v7
     def get_month(self, indate):
         new_date = datetime.strptime(indate, '%Y-%m-%d')
         out_date = new_date.strftime('%B') + '-' + new_date.strftime('%Y')
         return out_date
 
 
-class ReportStudentFeesRegister(osv.AbstractModel):
+class ReportStudentFeesRegister(models.AbstractModel):
     _name = 'report.school_fees.student_fees_register'
     _inherit = 'report.abstract_report'
     _template = 'school_fees.student_fees_register'
