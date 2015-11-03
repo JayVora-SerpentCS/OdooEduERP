862951c075d4b58ee57f172c1d96c97ae12ba5b3
serpentcs <support@serpentcs.com>
2015-11-03 12:52:45 +0530

    [IMP] EduERPv8 : Improved code in EduERP Modules.

--- exam/report/__init__.py
+++ exam/report/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import result_info
-import add_exam_result
+import exam.report.result_info
+import exam.report.add_exam_result
