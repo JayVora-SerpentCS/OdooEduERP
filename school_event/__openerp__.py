efdc680b16a18ccf3916f4b2e81cf32a272c949a
serpentcs <support@serpentcs.com>
2015-11-03 14:05:26 +0530

    [IMP] EduERPv8 : Improved code in EduERP Modules.

--- school_event/__openerp__.py
+++ school_event/__openerp__.py
@@ -1,25 +1,21 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-{
-    'name': 'School Event Management',
-    'version': '3.0',
-    'author': 'Serpent Consulting Services PVT. LTD.',
-    'website': 'http://www.serpentcs.com',
-    'category': 'School Management',
-    'license': '',
-    'complexity': 'easy',
-    'summary': 'A Module For Event Management In School',
-    'depends': ['school'],
-    'data': [
-             'views/event_view.xml',
-             'views/event_workflow.xml',
-             'security/event_security.xml',
-             'security/ir.model.access.csv',
-             'views/participants.xml',
-             'views/report_view.xml',
-            ],
-    'demo': ['demo/event_demo.xml'],
-    'installable': True,
-    'application': True,
-}
+{'name': 'School Event Management',
+ 'version': '3.0',
+ 'author': 'Serpent Consulting Services PVT. LTD.',
+ 'website': 'http://www.serpentcs.com',
+ 'category': 'School Management',
+ 'license': '',
+ 'complexity': 'easy',
+ 'summary': 'A Module For Event Management In School',
+ 'depends': ['school'],
+ 'data': ['views/event_view.xml',
+          'views/event_workflow.xml',
+          'security/event_security.xml',
+          'security/ir.model.access.csv',
+          'views/participants.xml',
+          'views/report_view.xml'],
+ 'demo': ['demo/event_demo.xml'],
+ 'installable': True,
+ 'application': True}
