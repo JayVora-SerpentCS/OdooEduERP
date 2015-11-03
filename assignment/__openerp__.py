41b48d7c8603a2028202e5dfc316bdda7dde1481
serpentcs <support@serpentcs.com>
2015-11-03 14:05:26 +0530

    [IMP] EduERPv8 : Improved code in EduERP Modules.

--- assignment/__openerp__.py
+++ assignment/__openerp__.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-{
-    'name': 'Assignment Management',
-    'version': '3.0',
-    'author': 'Serpent Consulting Services PVT. LTD.',
-    'website': 'http://www.serpentcs.com',
-    'license': '',
-    'category': 'School Management',
-    'summary': 'A Module For Assignment Management In School',
-    'complexity': 'easy',
-    'depends': ['school'],
-    'data': ['views/homework_view.xml', 'security/ir.model.access.csv'],
-    'demo': ['demo/assignment_demo.xml'],
-    'test': ['test/assignment.yml'],
-    'installable': True,
-    'application': True,
-}
+{'name': 'Assignment Management',
+ 'version': '3.0',
+ 'author': 'Serpent Consulting Services PVT. LTD.',
+ 'website': 'http://www.serpentcs.com',
+ 'license': '',
+ 'category': 'School Management',
+ 'summary': 'A Module For Assignment Management In School',
+ 'complexity': 'easy',
+ 'depends': ['school'],
+ 'data': ['views/homework_view.xml', 'security/ir.model.access.csv'],
+ 'demo': ['demo/assignment_demo.xml'],
+ 'test': ['test/assignment.yml'],
+ 'installable': True,
+ 'application': True}
