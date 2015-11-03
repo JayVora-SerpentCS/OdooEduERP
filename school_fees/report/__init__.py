1ad800a92fea3157787bf72985314b8d576e4278
serpentcs <support@serpentcs.com>
2015-11-03 12:52:45 +0530

    [IMP] EduERPv8 : Improved code in EduERP Modules.

--- school_fees/report/__init__.py
+++ school_fees/report/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import report_student_fees_register
-import report_student_payslip
+import school_fees.report.report_student_fees_register
+import school_fees.report.report_student_payslip
