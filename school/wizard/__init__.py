929749f683f75eb6ae89e32b69ab03f886e6b3c5
serpentcs <support@serpentcs.com>
2015-11-03 12:32:22 +0530

    [IMP] EduERPv8 : Improved code in EduERP Modules.

--- school/wizard/__init__.py
+++ school/wizard/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import assign_roll_no
-from . import move_standards
-from . import wiz_send_email
-from . import wiz_meeting
+import assign_roll_no
+import move_standards
+import wiz_send_email
+import wiz_meeting
