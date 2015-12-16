629e6157f49af8f63f8324f20b03f18c2f6b5e15
Anu Patel <a.patel.serpentcs@gmail.com>
2015-12-16 17:22:42 +0530

    Improved import statement in assignment.

--- assignment/__init__.py
+++ assignment/__init__.py
@@ -20,6 +20,6 @@
 #
 ##############################################################################
 
-import homework
+from . import homework
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
