43764331d4ecbb20787e8637bcaec36f66366f41
Anu Patel <a.patel.serpentcs@gmail.com>
2016-06-13 14:50:13 +0530

    [IMP] Improvements in in code as per travis standard. Removed tabs from xml files..

--- exam/__openerp__.py
+++ exam/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     "name": "Exam Management",
-    "version": "2.0",
+    "version": '8.0.2.0',
     "author": '''Odoo Community Association (OCA),
                  Serpent Consulting Services Pvt. Ltd.''',
     "license": 'AGPL-3',
