a9ea92f60d2eb0893cb2bcf80539ca0fde965445
Anu Patel <a.patel.serpentcs@gmail.com>
2016-09-03 10:48:34 +0530

    [FIX] Added improvements in init files to fix the instalation issue.

--- school/models/school.py
+++ school/models/school.py
@@ -6,7 +6,8 @@ import openerp
 from datetime import date, datetime
 from openerp import models, fields, api
 from openerp.tools.translate import _
-from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, image_colorize, image_resize_image_big
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
+from openerp.tools import image_colorize, image_resize_image_big
 from openerp.exceptions import except_orm, Warning as UserError
 
 
@@ -68,8 +69,8 @@ class AcademicYear(models.Model):
 
     @api.constrains('date_start', 'date_stop')
     def _check_duration(self):
-        if (self.date_stop and self.date_start
-                and self.date_stop < self.date_start):
+        if (self.date_stop and self.date_start and
+                self.date_stop < self.date_start):
             raise UserError(_('Error! The duration of the academic year\
                              is invalid.'))
 
@@ -92,19 +93,18 @@ class AcademicMonth(models.Model):
 
     @api.constrains('date_start', 'date_stop')
     def _check_duration(self):
-        if (self.date_stop
-                and self.date_start
-                and self.date_stop < self.date_start):
+        if (self.date_stop and self.date_start and
+                self.date_stop < self.date_start):
             raise UserError(_('Error ! The duration of the Month(s)\
                              is/are invalid.'))
 
     @api.constrains('year_id', 'date_start', 'date_stop')
     def _check_year_limit(self):
         if self.year_id and self.date_start and self.date_stop:
-            if (self.year_id.date_stop < self.date_stop
-                    or self.year_id.date_stop < self.date_start
-                    or self.year_id.date_start > self.date_start
-                    or self.year_id.date_start > self.date_stop):
+            if (self.year_id.date_stop < self.date_stop or
+                    self.year_id.date_stop < self.date_start or
+                    self.year_id.date_start > self.date_start or
+                    self.year_id.date_start > self.date_stop):
                 raise UserError(_('Invalid Months ! Some months overlap or\
                                    the date period is not in the scope of the\
                                    academic year.'))
@@ -198,9 +198,8 @@ class SchoolStandard(models.Model):
     def name_get(self):
         res = []
         for standard in self:
-            name = standard.standard_id.name\
-                   + "[" + standard.division_id.name\
-                   + "]"
+            name = (standard.standard_id.name + "[" +
+                    standard.division_id.name + "]")
             res.append((standard.id, name))
         return res
 
@@ -309,9 +308,11 @@ class StudentStudent(models.Model):
         return result
 
     @api.model
-    def _get_default_image(self, is_company, colorize=False):
-        image = image_colorize(open(openerp.modules.get_module_resource('base',
-                    'static/src/img', 'avatar.png')).read())
+    def _get_img(self, is_company, colorize=False):
+        avatar_img = openerp.modules.get_module_resource('base',
+                                                         'static/src/img',
+                                                         'avatar.png')
+        image = image_colorize(open(avatar_img).read())
         return image_resize_image_big(image.encode('base64'))
 
     user_id = fields.Many2one('res.users', 'User ID', ondelete="cascade",
@@ -327,10 +328,9 @@ class StudentStudent(models.Model):
     contact_phone1 = fields.Char('Phone no.',)
     contact_mobile1 = fields.Char('Mobile no',)
     roll_no = fields.Integer('Roll No.', readonly=True)
-    photo = fields.Binary('Photo', default=lambda self:\
-                          self._get_default_image
-                          (self._context.get('default_is_company',
-                                             False)))
+    photo = fields.Binary('Photo', default=lambda self:
+                          self._get_img(self._context.get('default_is_company',
+                                                          False)))
     year = fields.Many2one('academic.year', 'Academic Year', required=True,
                            states={'done': [('readonly', True)]})
     cast_id = fields.Many2one('student.cast', 'Religion')
@@ -474,15 +474,14 @@ class StudentStudent(models.Model):
                 self.write({'roll_no': number})
                 number += 1
             reg_code = self.env['ir.sequence'].get('student.registration')
-            registation_code = str(student_data.school_id.state_id.name)\
-                                + str('/') + str(student_data.school_id.city)\
-                                + str('/')\
-                                + str(student_data.school_id.name) + str('/')\
-                                + str(reg_code)
+            registation_code = (str(student_data.school_id.state_id.name) +
+                                str('/') + str(student_data.school_id.city) +
+                                str('/') + str(student_data.school_id.name) +
+                                str('/') + str(reg_code))
             stu_code = self.env['ir.sequence'].get('student.code')
-            student_code = str(student_data.school_id.code) + str('/')\
-                            + str(student_data.year.code) + str('/')\
-                            + str(stu_code)
+            student_code = (str(student_data.school_id.code) + str('/') +
+                            str(student_data.year.code) + str('/') +
+                            str(stu_code))
         self.write({'state': 'done',
                     'admission_date': time.strftime('%Y-%m-%d'),
                     'student_code': student_code,
