7386e6c37ee7ac9baea587899d505820bcf0b307
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2016-12-08 12:56:59 +0530

    [IMP]Improved Views,reports and methods as per v10

--- school_attendance/__manifest__.py
+++ school_attendance/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {'name': 'Attendance Management',
- 'version': '3.0',
+ 'version': '1.0',
  'author': 'Serpent Consulting Services PVT. LTD.',
  'website': 'http://www.serpentcs.com',
  'category': 'School Management',
