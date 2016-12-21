a561998c2158316499bbb375f93712800a01a67f
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-21 17:52:29 +0530

    [FIX]Fix the issues of barcode_report,library,timetable and schooll.

--- timetable/report/timetable_info.py
+++ timetable/report/timetable_info.py
@@ -9,8 +9,8 @@ from odoo import models, api
 class TimeTable(report_sxw.rml_parse):
 
     @api.multi
-    def __init__(self, cr, uid, name, context=None):
-        super(TimeTable, self).__init__(cr, uid, name, context=context)
+    def __init__(self):
+        super(TimeTable, self).__init__()
         self.localcontext.update({'time': time,
                                   'get_timetable': self._get_timetable})
 
@@ -23,11 +23,11 @@ class TimeTable(report_sxw.rml_parse):
                          t.subject_id= s.id and t.teacher_id= hr.id\
                          and hr.resource_id = r.id  and table_id = %d\
                          group by start_time,end_time,s.name,week_day,r.name\
-                         order by start_time" % (timetable_id.id))
+                         order by start_time" %(timetable_id.id))
         res = self.cr.dictfetchall()
         self.cr.execute("select start_time,end_time from time_table_line\
                          where table_id=%d group by start_time,end_time\
-                         order by start_time" % (timetable_id.id))
+                         order by start_time" %(timetable_id.id))
         time_data = self.cr.dictfetchall()
         for time_detail in time_data:
             for data in res:
