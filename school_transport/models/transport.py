86da6310cc0a7e4da414d5b3aa8fd0f755c84e51
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-22 12:39:16 +0530

    [FIX]To fixes the PEP 8 Error

--- school_transport/models/transport.py
+++ school_transport/models/transport.py
@@ -202,7 +202,7 @@ class StudentTransports(models.Model):
                 prt_obj.write(prt_data.id, {'state': 'over'})
 
             if participants:
-                self.write(trans.id, {'trans_participants_ids': 
+                self.write(trans.id, {'trans_participants_ids':
                                       [(6, 0, participants)]},)
 
         for vehicle in vehi_obj.browse(vehi_ids):
@@ -274,8 +274,8 @@ class TransportRegistration(models.Model):
         if not month:
             return {}
         tr_start_date = time.strftime("%Y-%m-%d")
-        tr_end_date = datetime.strptime(tr_start_date, '%Y-%m-%d')+\
-                      relativedelta(months=+month)
+        mon = relativedelta(months=+month)
+        tr_end_date = datetime.strptime(tr_start_date, '%Y-%m-%d') + mon
         date = datetime.strftime(tr_end_date, '%Y-%m-%d')
         return {'value': {'reg_end_date': date}}
 
@@ -305,8 +305,8 @@ class TransportRegistration(models.Model):
             amount = reg_data.point_id.amount * reg_data.for_month
             tr_start_date = (reg_data.reg_date)
             month = reg_data.for_month
-            tr_end_date = datetime.strptime(tr_start_date, '%Y-%m-%d')+\
-                          relativedelta(months=+month)
+            mon1 = relativedelta(months=+month)
+            tr_end_date = datetime.strptime(tr_start_date, '%Y-%m-%d') + mon1
             date = datetime.strptime(reg_data.name.end_date, '%Y-%m-%d')
             if tr_end_date > date:
                 raise UserError(_('For this much Months\
