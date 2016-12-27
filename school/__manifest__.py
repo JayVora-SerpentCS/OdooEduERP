e256ae6bca83bea56095f0246862f7855f813ab0
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-27 17:58:54 +0530

    [ADD]Added Odoo SA in author

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -3,7 +3,7 @@
 
 {'name': 'School',
  'version': "10.0.1.0.0",
- 'author': "Serpent Consulting Services Pvt. Ltd., OpenERP SA",
+ 'author': "Serpent Consulting Services Pvt. Ltd., Odoo SA",
  'website': 'http://www.serpentcs.com',
  'category': 'School Management',
  'license': "AGPL-3",
