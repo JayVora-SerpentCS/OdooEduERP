cb010fb2cc18ca50238dc5f92417b814a2754717
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-27 12:47:03 +0530

    [FIX]Solve Datetime Format Issues and Manifest File in barcode_report.

--- barcode_report/__manifest__.py
+++ barcode_report/__manifest__.py
@@ -10,7 +10,6 @@
  'summary': 'A Module For BarCode System For School',
  'depends': ['timetable', 'exam'],
  'data': ['views/result_report.xml',
-#           'views/result_label.xml',
-#           'views/result_lable_info.xml'
-],
+          'views/result_label.xml',
+          'views/result_lable_info.xml'],
  'installable': True}
