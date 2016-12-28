aa9611ba6bcecd4a1c84de35098c9a27a8843498
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-28 10:34:42 +0530

    [FIX]Fix the testing flake 8 error.

--- school_hostel/report/hostel_fee_receipt.py
+++ school_hostel/report/hostel_fee_receipt.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# See LICENSE file for full copyright and licensing details.
 #
 from odoo.report import report_sxw
 from odoo import models
