bfc4798a94fb863566e137017dcf972563d02593
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-28 10:34:42 +0530

    [FIX]Fix the testing flake 8 error.

--- exam/report/__init__.py
+++ exam/report/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# See LICENSE file for full copyright and licensing details.
 
 # from . import result_info
 # from . import add_exam_result
