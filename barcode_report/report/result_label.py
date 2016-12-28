9c1d92717f25a79de9c7ebe38dd31d51a44d540f
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-28 11:13:57 +0530

    [FIX]Fix the travis error.

--- barcode_report/report/result_label.py
+++ barcode_report/report/result_label.py
@@ -1,58 +1,33 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
+
 from odoo import models, api
-import odoo
+from odoo.report import report_sxw
+
+
+class ResultLabel(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context=None):
+        super(ResultLabel, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({'get_student_info': self.get_student_info})
+
+    def get_student_info(self, standard_id, division_id, medium_id, year_id):
+        student_obj = self.pool.get('student.student')
+        student_ids = student_obj.search(self.cr, self.uid,
+                                         [('standard_id', '=', standard_id),
+                                          ('division_id', '=', division_id),
+                                          ('medium_id', '=', medium_id),
+                                          ('year', '=', year_id)])
+        result = []
+        for student in student_obj.browse(self.cr, self.uid, student_ids):
+            result.append(student.pid)
+        return result
 
 
 class ReportLabel(models.AbstractModel):
 
     _name = 'report.barcode_report.result_label'
-
-    def __init__(self):
-        super(ResultLabel, self).__init__()
-        self.localcontext.update({'get_student_info': self.get_student_info})
-
-    def get_student_info(self, standard_id, division_id, medium_id, year_id):
-        env = odoo.api.Environment(self.cr, self.uid, {})
-        student_obj = env['student.student']
-        student_ids = student_obj.search([('standard_id', '=', standard_id),
-                                          ('division_id', '=', division_id),
-                                          ('medium_id', '=', medium_id),
-                                          ('year', '=', year_id)])
-        result = []
-        for student in student_ids:
-            result.append(student.pid)
-        return result
-
-    @api.model
-    def render_html(self, docids, data=None):
-        self.model = self.env.context.get('active_model')
-        docs = self.env[self.model].browse(self.env.context.get('active_ids',
-                                                                []))
-        standard_id = data.get('standard_id')
-        division_id = data.get('division_id')
-        medium_id = data.get('medium_id')
-        year_id = data.get('year_id')
-        rm_act = self.with_context(data['form'].get('used_context', {}))
-        get_student_info = rm_act.get_student_info(standard_id, division_id,
-                                                   medium_id, year_id)
-        docargs = {
-            'doc_ids': docids,
-            'doc_model': self.model,
-            'data': data['form'],
-            'docs': docs,
-            'time': time,
-            'GetData': get_data_res,
-            'GetReserv': get_reserv_res,
-        }
-        standard_id = parser.parse(docargs.get('data').get('standard_id '))
-        docargs['data'].update({'standard_id': standard_id})
-        division_id = parser.parse(docargs.get('data').get('division_id '))
-        docargs['data'].update({'division_id': division_id})
-        medium_id = parser.parse(docargs.get('data').get('medium_id '))
-        docargs['data'].update({'medium_id': medium_id})
-        year_id = parser.parse(docargs.get('data').get('year_id '))
-        docargs['data'].update({'year_id': year_id})
-        render_model = 'barcode_report.result_label'
-        return self.env['report'].render(render_model, docargs)
+    _inherit = 'report.abstract_report'
+    _template = 'barcode_report.result_label'
+    _wrapped_report_class = ResultLabel
--- barcode_report/report/result_label_info.py
+++ barcode_report/report/result_label_info.py
@@ -1,62 +1,37 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-import time
-from dateutil import parser
+
+from odoo.report import report_sxw
 from odoo import api, fields, models
+
+
+class ResultLabelInfo(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context=None):
+        super(ResultLabelInfo, self).__init__(cr, uid, name, context=context)
+        get_stud_info = self.get_student_info
+        self.localcontext.update({'get_student_all_info': get_stud_info})
+
+    def get_student_info(self, standard_id, division_id, medium_id, year_id):
+        student_obj = self.pool.get('student.student')
+        student_ids = student_obj.search(self.cr, self.uid,
+                                         [('standard_id', '=', standard_id),
+                                          ('division_id', '=', division_id),
+                                          ('medium_id', '=', medium_id),
+                                          ('year', '=', year_id)])
+        result = []
+        for student in student_obj.browse(self.cr, self.uid, student_ids):
+            name = (student.name + " " + student.middle or '' + " " +
+                    student.last or '')
+            result.append({'name': name,
+                           'roll_no': student.roll_no, 'pid': student.pid})
+        return result
 
 
 class ReportLabelInfo(models.AbstractModel):
 
     _name = 'report.barcode_report.result_label_info'
-
-    def __init__(self):
-        super(ResultLabelInfo, self).__init__()
-        get_stud_info = self.get_student_info
-        self.with_context.update({'get_student_all_info': get_stud_info})
-
-    def get_student_info(self, standard_id, division_id, medium_id, year_id):
-        env = odoo.api.Environment(self.cr, self.uid, {})
-        student_obj = env['student.student']
-        student_ids = student_obj.search([('standard_id', '=', standard_id),
-                                          ('division_id', '=', division_id),
-                                          ('medium_id', '=', medium_id),
-                                          ('year', '=', year_id)])
-        result = []
-        for stud in student_ids:
-            name = stud.name + " " + stud.middle or '' + " " + stud.last or ''
-            result.append({'name': name,
-                           'roll_no': stud.roll_no, 'pid': stud.pid})
-        return result
-
-    @api.model
-    def render_html(self, docids, data=None):
-        self.model = self.env.context.get('active_model')
-        docs = self.env[self.model].browse(self.env.context.get('active_ids',
-                                                                []))
-        standard_id = data.get('standard_id')
-        division_id = data.get('division_id')
-        medium_id = data.get('medium_id')
-        year_id = data.get('year_id')
-        rm_act = self.with_context(data['form'].get('used_context', {}))
-        get_student_info = rm_act.get_student_info(standard_id, division_id,
-                                                   medium_id, year_id)
-        docargs = {
-            'doc_ids': docids,
-            'doc_model': self.model,
-            'data': data['form'],
-            'docs': docs,
-            'time': time,
-            'GetData': get_data_res,
-            'GetReserv': get_reserv_res,
-        }
-        standard_id = parser.parse(docargs.get('data').get('standard_id '))
-        docargs['data'].update({'standard_id': standard_id})
-        division_id = parser.parse(docargs.get('data').get('division_id '))
-        docargs['data'].update({'division_id': division_id})
-        medium_id = parser.parse(docargs.get('data').get('medium_id '))
-        docargs['data'].update({'medium_id': medium_id})
-        year_id = parser.parse(docargs.get('data').get('year_id '))
-        docargs['data'].update({'year_id': year_id})
-        render_model = 'barcode_report.result_label_info'
-        return self.env['report'].render(render_model, docargs)
+    _inherit = 'report.abstract_report'
+    _template = 'barcode_report.result_label_info'
+    _wrapped_report_class = ResultLabelInfo
