e7b76590309ce1259fb01de58b3d95a6b696a839
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-29 13:09:39 +0530

    [IMP]Improve barcode_report and timetable report as per v10 standard

--- timetable/report/__init__.py
+++ timetable/report/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-# from . import timetable_info
+from . import timetable_info
--- timetable/report/timetable_info.py
+++ timetable/report/timetable_info.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-import time
 from odoo import models, api
 
 
@@ -11,18 +10,18 @@ class ReportTimetableInfo(models.AbstractModel):
     @api.multi
     def _get_timetable(self, timetable_id):
         timetable_detail = []
-        self.cr.execute("select t.start_time,t.end_time,s.name,week_day,r.name\
-                         as teacher from time_table_line t, subject_subject s,\
-                         resource_resource r, hr_employee hr where\
-                         t.subject_id= s.id and t.teacher_id= hr.id\
+        self._cr.execute("select t.start_time,t.end_time,s.name,week_day,\
+                         r.name as teacher from time_table_line t,\
+                         subject_subject s, resource_resource r, hr_employee\
+                         hr where t.subject_id= s.id and t.teacher_id= hr.id\
                          and hr.resource_id = r.id  and table_id = %d\
                          group by start_time,end_time,s.name,week_day,r.name\
                          order by start_time" % (timetable_id.id))
-        res = self.cr.dictfetchall()
-        self.cr.execute("select start_time,end_time from time_table_line\
+        res = self._cr.dictfetchall()
+        self._cr.execute("select start_time,end_time from time_table_line\
                          where table_id=%d group by start_time,end_time\
                          order by start_time" % (timetable_id.id))
-        time_data = self.cr.dictfetchall()
+        time_data = self._cr.dictfetchall()
         for time_detail in time_data:
             for data in res:
                 if ((time_detail['start_time'] == data['start_time']) and
@@ -37,20 +36,12 @@ class ReportTimetableInfo(models.AbstractModel):
 
     @api.model
     def render_html(self, docids, data=None):
-        self.model = self.env.context.get('active_model')
-
-        docs = self.env[self.model].browse(self.env.context.get('active_ids',
-                                                                []))
-        timetable_id = data['form'].get('timetable_id')[0]
-        get_student = self.with_context(data['form'].get('used_context', {}))
-        _get_timetable = get_student._get_timetable(timetable_id)
+        docs = self.env['time.table'].browse(docids)
         docargs = {
             'doc_ids': docids,
-            'doc_model': self.model,
-            'data': data['form'],
+            'doc_model': self.env['time.table'],
+            'data': data,
             'docs': docs,
-            'time': time,
-            '_get_timetable': _get_timetable,
+            'get_timetable': self._get_timetable,
         }
-        render_model = 'timetable.timetable'
-        return self.env['report'].render(render_model, docargs)
+        return self.env['report'].render('timetable.timetable', docargs)
