2a67f732c427ef9dcc0b6efac115818205b3dd22
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-30 17:31:36 +0530

    [FIX]Fix the travis

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -2,8 +2,8 @@
 # See LICENSE file for full copyright and licensing details.
 
 {'name': 'School',
- 'version': "10.0.1.0.0",
- 'author': "Serpent Consulting Services Pvt. Ltd., ODOO SA",
+ 'version': '10.0.1.0.0',
+ 'author': 'Serpent Consulting Services Pvt. Ltd., ODOO SA',
  'website': 'http://www.serpentcs.com',
  'category': 'School Management',
  'license': "AGPL-3",
