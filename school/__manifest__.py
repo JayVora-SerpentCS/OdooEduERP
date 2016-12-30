3beb7ea713fdc8a6dba3994440f99d31cff85feb
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-12-30 16:55:52 +0530

    [IMP]Change version as per v10 standard

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -2,8 +2,8 @@
 # See LICENSE file for full copyright and licensing details.
 
 {'name': 'School',
- 'version': "10.0.1.0.0",
- 'author': "Serpent Consulting Services Pvt. Ltd., Odoo SA",
+ 'version': '10.0.1.0.0',
+ 'author': 'Serpent Consulting Services Pvt. Ltd., OpenERP SA',
  'website': 'http://www.serpentcs.com',
  'category': 'School Management',
  'license': "AGPL-3",
