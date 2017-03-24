8f1e670d216131e9b130c023c6551f2031f118b3
Arpit Joshi <arpit.joshi.serpentcs@gmail.com>
2017-03-24 19:58:51 +0530

    [IMP]Added Dynamic Date files and did view level changes.

--- assignment/__manifest__.py
+++ assignment/__manifest__.py
@@ -12,6 +12,7 @@
  'depends': ['school'],
  'data': ['views/homework_view.xml', 'security/ir.model.access.csv'],
  'test': ['test/assignment.yml'],
+ 'demo': ['demo/assignment_demo.xml'],
  'installable': True,
  'application': True,
  'auto_install': False}
