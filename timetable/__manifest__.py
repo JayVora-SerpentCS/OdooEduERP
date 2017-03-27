d972ff449fa213da1d26727f264ef45f111bb9d0
Arpit Joshi <arpit.joshi.serpentcs@gmail.com>
2017-03-27 19:26:32 +0530

    Improve code for dynamic date for academic year.Changes in the compute field.

--- timetable/__manifest__.py
+++ timetable/__manifest__.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-{'name': 'Timetable Management',
- 'version': '10.0.1.0.0',
- 'author': 'Serpent Consulting Services Pvt. Ltd., Odoo SA',
- 'website': 'http://www.serpentcs.com',
- 'license': "AGPL-3",
- 'category': 'School Management',
- 'complexity': 'easy',
- 'summary': 'A Module For Timetable Management In School',
- 'depends': ['school'],
- 'data': ['security/ir.model.access.csv',
-          'views/timetable_view.xml',
-          'views/report_view.xml',
-          'views/timetable.xml'],
- 'demo': ['demo/timetable_demo.xml'],         
- 'test': ['test/timetable.yml'],
- 'installable': True,
- 'application': True}
+{
+    'name': 'Timetable Management',
+    'version': '10.0.1.0.0',
+    'author': 'Serpent Consulting Services Pvt. Ltd., Odoo SA',
+    'website': 'http://www.serpentcs.com',
+    'license': "AGPL-3",
+    'category': 'School Management',
+    'complexity': 'easy',
+    'summary': 'A Module For Timetable Management In School',
+    'depends': ['school'],
+    'data': ['security/ir.model.access.csv',
+             'views/timetable_view.xml',
+             'views/report_view.xml',
+             'views/timetable.xml'],
+    'demo': ['demo/timetable_demo.xml'],
+    'test': ['test/timetable.yml'],
+    'installable': True,
+    'application': True
+}
