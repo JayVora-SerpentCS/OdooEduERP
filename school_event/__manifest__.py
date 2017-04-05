1a85be3189c2041b59d45fe96b341ddd431ea45a
Arpit Joshi <arpit.joshi.serpentcs@gmail.com>
2017-04-05 19:39:38 +0530

    Improve code in py and manifest file.

--- school_event/__manifest__.py
+++ school_event/__manifest__.py
@@ -4,7 +4,8 @@
 {
     'name': 'School Event Management',
     'version': "10.0.1.0.0",
-    'author': "Serpent Consulting Services Pvt. Ltd., Odoo SA",
+    'author': '''Serpent Consulting Services Pvt. Ltd.,
+                 Odoo Community Association (OCA)''',
     'website': 'http://www.serpentcs.com',
     'category': 'School Management',
     'license': "AGPL-3",
