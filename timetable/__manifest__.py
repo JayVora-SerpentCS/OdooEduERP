35650d25bf835fcaaf741bd271afe9825451b471
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2017-04-20 15:24:52 +0530

    [ADD/IMP]:added and improvement the security

--- timetable/__manifest__.py
+++ timetable/__manifest__.py
@@ -16,7 +16,7 @@
              'views/timetable_view.xml',
              'views/report_view.xml',
              'views/timetable.xml'],
-    'demo': ['demo/timetable_demo.xml'],
+#    'demo': ['demo/timetable_demo.xml'],
     'test': ['test/timetable.yml'],
     'installable': True,
     'application': True
