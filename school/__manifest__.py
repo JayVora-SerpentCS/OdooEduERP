e134d210d4502889567afc14029cc3c9f139bdd6
arpit.joshi.serpentcs@gmail.com <Arpit Joshi>
2017-04-24 16:18:02 +0530

    [IMP]:add change log in manifest file..

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'School',
-    'version': '1.0.3',
+    'version': '1.0.4',
     'author': '''Serpent Consulting Services Pvt. Ltd.,
                  Odoo Community Association (OCA)''',
     'website': 'http://www.serpentcs.com',
