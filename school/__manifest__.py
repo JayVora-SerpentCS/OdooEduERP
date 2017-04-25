0822b4809b4b4b3d342307d42dc807a18c5c0116
arpit.joshi.serpentcs@gmail.com <Arpit Joshi>
2017-04-25 13:16:00 +0530

    [IMP]Changes in the address view of partner and add groups to the report.

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'School',
-    'version': '1.0.4',
+    'version': '1.0.5',
     'author': '''Serpent Consulting Services Pvt. Ltd.,
                  Odoo Community Association (OCA)''',
     'website': 'http://www.serpentcs.com',
