0b5ed6d72f111c91181e28c3fba265781e0198d2
Anu Patel <a.patel.serpentcs@gmail.com>
2017-05-09 16:01:19 +0530

    [IMP] Added improvements for timetable demo data

--- school/wizard/move_standards.py
+++ school/wizard/move_standards.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api, _
-from odoo.exceptions import except_orm
+from odoo import models, fields, api
 
 
 class MoveStandards(models.TransientModel):
@@ -15,38 +14,40 @@ class MoveStandards(models.TransientModel):
     def move_start(self):
         '''Code for moving student to next standard'''
         academic_obj = self.env['academic.year']
-        school_standard_obj = self.env['school.standard']
+        school_stand_obj = self.env['school.standard']
         standard_obj = self.env["standard.standard"]
         stud_history_obj = self.env["student.history"]
         student_obj = self.env['student.student']
-        for data in self:
-            students = student_obj.search([('state', '=', 'done')])
-            for student in students:
+        for rec in self:
+            for student in student_obj.search([('state', '=', 'done')]):
                 year_id = academic_obj.next_year(student.year.sequence)
                 # Check if academic year selected or not.
-                if year_id != data.academic_year_id.id:
+                if year_id != rec.academic_year_id.id:
                     continue
                 standard_seq = student.standard_id.standard_id.sequence
                 next_class_id = standard_obj.next_standard(standard_seq)
 
                 # Assign the academic year
                 if next_class_id:
-                    next_school_standard = school_standard_obj.search(
-                            [('standard_id', '=', next_class_id),
-                            ('division_id', '=', student.division_id.id or
-                                        student.standard_id.division_id.id),
-                             ('school_id', '=', student.school_id.id),
-                             ('medium_id', '=', student.medium_id.id)])
-                    std_vals = {'year': data.academic_year_id.id or False,
-                            'standard_id': next_school_standard.id or False}
+                    division = (student.division_id.id or
+                                student.standard_id.division_id.id or False)
+                    next_stand = school_stand_obj.search([('standard_id', '=',
+                                                           next_class_id),
+                                                          ('division_id', '=',
+                                                           division),
+                                                          ('school_id', '=',
+                                                           student.school_id.id),
+                                                          ('medium_id', '=',
+                                                           student.medium_id.id)])
+                    std_vals = {'year': rec.academic_year_id.id or False,
+                                'standard_id': next_stand.id or False}
                     # Move student to next standard
                     student.write(std_vals)
                     vals = {'student_id': student.id,
                             'academice_year_id': student.year.id,
                             'standard_id': student.standard_id.id,
                             'medium_id': student.medium_id.id,
-                            'division_id': student.division_id.id,
-                         }
+                            'division_id': student.division_id.id}
                     stud_history_obj.create(vals)
         return True
 
