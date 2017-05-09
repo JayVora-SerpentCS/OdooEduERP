29979b396dd9c9c790257aa6d399b455efbda986
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2017-05-09 16:28:46 +0530

    [IMP]:Add improvements in all fields to make it only selectable

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'School',
-    'version': '10.0.1.0.11',
+    'version': '10.0.1.0.12',
     'author': '''Serpent Consulting Services Pvt. Ltd.,
                  Odoo Community Association (OCA)''',
     'website': 'http://www.serpentcs.com',
