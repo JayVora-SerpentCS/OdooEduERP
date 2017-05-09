efbe5ef78b854afd4b3828b31f87aa29c68f4e47
Anu Patel <a.patel.serpentcs@gmail.com>
2017-05-09 16:01:19 +0530

    [IMP] Added improvements for timetable demo data

--- timetable/models/timetable.py
+++ timetable/models/timetable.py
@@ -39,10 +39,11 @@ class TimeTable(models.Model):
                            rec.end_time == rec_check.end_time and
                            rec.teacher_id.id == rec.teacher_id.id)]
             if len(records) > 1:
-                raise ValidationError(_('''You can Not set lecture at same time
-                %s  at same day %s of teacher %s..!!!''')
-                                % (rec.start_time, rec.week_day,
-                                   rec.teacher_id.name))
+                raise ValidationError(_('''You can Not set lecture at same
+                                        time %s  at same day %s of teacher
+                                        %s..!!!''') % (rec.start_time,
+                                                       rec.week_day,
+                                                       rec.teacher_id.name))
             # Checks if time is greater than 24 hours than raise error
             if rec.start_time > 24:
                 raise UserError(_('Start Time should be less than 24 hours'))
