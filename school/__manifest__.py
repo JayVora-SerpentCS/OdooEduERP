352e9dba9e9fdae166db20d1f49f410188064411
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2017-05-09 15:10:41 +0530

    [IMP]:added the menu of the move standard and assign roll no and optimized code

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -2,7 +2,7 @@
 # See LICENSE file for full copyright and licensing details.
 {
     'name': 'School',
-    'version': '10.0.1.0.10',
+    'version': '10.0.1.0.11',
     'author': '''Serpent Consulting Services Pvt. Ltd.,
                  Odoo Community Association (OCA)''',
     'website': 'http://www.serpentcs.com',
--- school/wizard/assign_roll_no.py
+++ school/wizard/assign_roll_no.py
@@ -10,27 +10,23 @@ class AssignRollNo(models.TransientModel):
     _name = 'assign.roll.no'
     _description = 'Assign Roll Number'
 
-    standard_id = fields.Many2one('standard.standard', 'Class', required=True)
+    standard_id = fields.Many2one('school.standard', 'Class', required=True)
     medium_id = fields.Many2one('standard.medium', 'Medium', required=True)
-    division_id = fields.Many2one('standard.division', 'Division',
-                                  required=True)
 
     @api.multi
     def assign_rollno(self):
         '''Method to assign roll no to students'''
         student_obj = self.env['student.student']
         # Search Student
-        for rec in self:
-            student_ids = student_obj.search([('standard_id', '=',
-                                               rec.standard_id.id),
-                                              ('medium_id', '=',
-                                               rec.medium_id.id),
-                                              ('division_id', '=',
-                                               rec.division_id.id)],
-                                             order="name")
-            # Assign roll no according to name.
-            number = 1
-            for student in student_ids:
-                number += 1
-                student.write({'roll_no': number})
+        for student_data in self:
+            search_student_ids = student_obj.search([
+                ('standard_id', '=', student_data.standard_id.id),
+                ('medium_id', '=', student_data.medium_id.id)],
+                                                    order="name")
+        # Assign roll no according to name.
+        number = 1
+        for student in search_student_ids:
+            number += 1
+            student.write({'roll_no': number})
+
         return True
