f066022c2ad63b53e30b73424c4a13f3c40d8123
arpit.joshi.serpentcs@gmail.com <Arpit Joshi>
2017-05-10 18:03:36 +0530

    [IMP] View level changes in exam module.

--- exam/__manifest__.py
+++ exam/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Exam Management',
-    'version': "10.0.1.0.7",
+    'version': "10.0.1.0.8",
     'author': '''Serpent Consulting Services Pvt. Ltd.,
                  Odoo Community Association (OCA)''',
     'website': 'http://www.serpentcs.com',
