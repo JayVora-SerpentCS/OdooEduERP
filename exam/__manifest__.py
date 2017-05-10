ac84831d03a8c3df3d40c32edf3c99b7bfc4de2c
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2017-05-10 16:17:16 +0530

    [IMP]:added the code of the move standard wizard based on the exam result

--- exam/__manifest__.py
+++ exam/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Exam Management',
-    'version': "1.0.6",
+    'version': "10.0.1.0.7",
     'author': '''Serpent Consulting Services Pvt. Ltd.,
                  Odoo Community Association (OCA)''',
     'website': 'http://www.serpentcs.com',
