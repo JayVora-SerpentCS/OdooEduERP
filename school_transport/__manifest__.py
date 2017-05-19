65704bb89c419ef6cc74d94bef2aecf3db4bd744
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2017-05-19 12:58:25 +0530

    [IMP]:Code optimized of the transport module

--- school_transport/__manifest__.py
+++ school_transport/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Transport Management',
-    'version': "10.0.1.0.3",
+    'version': "10.0.1.0.4",
     'author': '''Serpent Consulting Services Pvt. Ltd.''',
     'website': 'http://www.serpentcs.com',
     'license': "AGPL-3",
