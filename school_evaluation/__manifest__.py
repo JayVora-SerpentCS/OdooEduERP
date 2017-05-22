6194987b5628b9f92afd2b0ba34f5d2ac9264c97
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2017-05-22 18:01:56 +0530

    [IMP]:Improvement in the school evaluation like remove object of the teacher and improvement also

---
+++ school_evaluation/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Evaluation Management',
+    'version': "10.0.1.0.3",
+    'author': '''Serpent Consulting Services Pvt. Ltd.''',
+    'website': 'http://www.almisnadtechnology.com',
+    'category': 'School Management',
+    'license': "AGPL-3",
+    'complexity': 'easy',
+    'summary': 'A Module For Evaluation Management In School',
+    'depends': ['hr', 'school'],
+    'data': ['security/evaluation_security.xml',
+             'views/school_evaluation_view.xml',
+             'views/templates.xml',
+             'security/ir.model.access.csv'],
+    'demo': ['demo/school_evaluation_demo.xml'],
+    'installable': True,
+    'application': True
+}
