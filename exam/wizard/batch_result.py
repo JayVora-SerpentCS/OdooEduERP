71045563fc2adaba9410cdcb854dfd82285c8cf9
arpit.joshi.serpentcs@gmail.com <arpit.joshi.serpentcs@gmail.com>
2017-05-26 12:31:05 +0530

    [IMP]Remove print statement

--- exam/wizard/batch_result.py
+++ exam/wizard/batch_result.py
@@ -16,6 +16,5 @@ class BatchExamResult(models.TransientModel):
     @api.multi
     def print_batch_report(self):
         data = self.read()[0]
-        print"batch data+++++", data
         return self.env['report'].get_action(self, 'exam.exam_result_batch',
                                              data=data)
