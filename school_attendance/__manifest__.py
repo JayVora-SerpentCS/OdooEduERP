5a80ac13414c13f016cddb6f78c52d238353fc7b
Tejas Chaudhari <tejas.c.serpentcs@gmail.com>
2017-05-30 18:00:53 +0530

    [ADD] Added banner and icon to several modules.

--- school_attendance/__manifest__.py
+++ school_attendance/__manifest__.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-{'name': 'Attendance Management',
- 'version': "10.0.1.0.5",
- 'author': "Serpent Consulting Services Pvt. Ltd.",
- 'website': 'http://www.serpentcs.com',
- 'category': 'School Management',
- 'license': "AGPL-3",
- 'summary': 'A Module For Attendance Management In School',
- 'complexity': 'easy',
- 'depends': ['school', 'hr'],
- 'data': ['security/attendance_security.xml',
-          'security/ir.model.access.csv',
-          'views/school_attendance_view.xml',
-          'views/month_attendance.xml',
-          'views/report_view.xml',
-          'wizard/attendance_sheet_wizard_view.xml',
-          'wizard/student_attendance_by_month_view.xml'],
- 'demo': ['demo/school_attendance_demo.xml'],
- 'installable': True,
- 'application': True}
+{
+    'name': 'Attendance Management',
+    'version': "10.0.1.0.0",
+    'author': '''Serpent Consulting Services Pvt. Ltd.,
+                 Odoo Community Association (OCA)''',
+    'website': 'http://www.serpentcs.com',
+    'images': ['static/description/SchoolAttendance.png'],
+    'category': 'School Management',
+    'license': "AGPL-3",
+    'summary': 'A Module For Attendance Management In School',
+    'complexity': 'easy',
+    'depends': ['school', 'hr'],
+    'data': ['security/ir.model.access.csv',
+             'views/school_attendance_view.xml',
+             'views/attendance_workflow.xml',
+             'wizard/attendance_sheet_wizard_view.xml',
+             'wizard/student_attendance_by_month_view.xml'],
+    'demo': ['demo/school_attendance_demo.xml'],
+    'installable': True,
+    'application': True
+}
