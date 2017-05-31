8e3a823fb6be99ab2033b643b1dae529a30c880b
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2017-05-31 19:16:45 +0530

    [IMP]:fixed the conflict

--- school_event/__manifest__.py
+++ school_event/__manifest__.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-{'name': 'School Event Management',
- 'version': "10.0.1.0.3",
- 'author': "Serpent Consulting Services Pvt. Ltd.",
- 'website': 'http://www.serpentcs.com',
- 'category': 'School Management',
- 'license': "AGPL-3",
- 'complexity': 'easy',
- 'summary': 'A Module For Event Management In School',
- 'depends': ['school'],
- 'data': ['security/event_security.xml',
-          'security/ir.model.access.csv',
-          'views/event_view.xml',
-          'views/participants.xml',
-          'views/report_view.xml'],
+{
+    'name': 'School Event Management',
+    'version': "10.0.1.0.3",
+    'author': '''Serpent Consulting Services Pvt. Ltd.,
+                 Odoo Community Association (OCA)''',
+    'website': 'http://www.serpentcs.com',
+    'images': ['static/description/SchoolEvent.png'],
+    'category': 'School Management',
+    'license': "AGPL-3",
+    'complexity': 'easy',
+    'summary': 'A Module For Event Management In School',
+    'depends': ['school'],
+    'data': ['security/event_security.xml',
+             'security/ir.model.access.csv',
+             'views/event_view.xml',
+             'views/participants.xml',
+             'views/report_view.xml'],
     'demo': ['demo/event_demo.xml'],
- 'installable': True,
- 'application': True}
+    'installable': True,
+    'application': True
+}
