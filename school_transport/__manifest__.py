0ca9d93bde8c446ead50a3d7a42a83cc3f8f059e
arpit.joshi.serpentcs@gmail.com <arpit.joshi.serpentcs@gmail.com>
2017-06-06 16:58:44 +0530

    [ADD] added test cases

--- school_transport/__manifest__.py
+++ school_transport/__manifest__.py
@@ -21,7 +21,6 @@
              'data/transport_schedular.xml',
              'wizard/transfer_vehicle.xml'],
     'demo': ['demo/transport_demo.xml'],
-#    'test': ['test/transport.yml'],
     'installable': True,
     'application': True
 }
