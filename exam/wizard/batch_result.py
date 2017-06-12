b60742391c9bbe8af0b34ffb79cee12f8798db3e
arpit.joshi.serpentcs@gmail.com <arpit.joshi.serpentcs@gmail.com>
2017-06-12 19:28:22 +0530

    [ADD] added tool tip and security changes

--- exam/wizard/batch_result.py
+++ exam/wizard/batch_result.py
@@ -10,8 +10,10 @@ class BatchExamResult(models.TransientModel):
     _name = "exam.batchwise.result"
 
     standard_id = fields.Many2one("school.standard",
-                                  "Standard")
-    year = fields.Many2one('academic.year', 'Academic Year')
+                                  "Standard",
+                                  help="select standard")
+    year = fields.Many2one('academic.year', 'Academic Year',
+                           help="Select Academic Year")
 
     @api.multi
     def print_batch_report(self):
