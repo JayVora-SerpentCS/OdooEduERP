b9ad929aac62459c66ecf75cc4b2cb43bbba9d0c
arpit.joshi.serpentcs@gmail.com <Arpit Joshi>
2017-06-13 12:15:05 +0530

    [IMP] fix travis error

--- OdooEduErp/models/demo.py
+++ OdooEduErp/models/demo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api, _
+from odoo import models, fields
 
 
 class DemoInstall(models.Model):
