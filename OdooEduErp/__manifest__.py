f3bcfb898bdff7d0ddf9a250d86a5298f60b7872
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-07-24 16:18:36 +0530

    [ADD]Added index file and update __manifest__.py file in OdooEduERP.

--- OdooEduErp/__manifest__.py
+++ OdooEduErp/__manifest__.py
@@ -3,17 +3,16 @@
 
 {
     'name': 'Education ERP',
-    'version': "10.0.1.0.1",
+    'version': '10.0.1.0.1',
     'author': '''Serpent Consulting Services Pvt. Ltd.''',
     'website': 'http://www.serpentcs.com',
     'category': 'EduErp',
-    'license': "AGPL-3",
+    'license': 'AGPL-3',
     'complexity': 'easy',
     'summary': 'Install all the modules',
-    'depends': ['school', 'assignment', 'exam', 'library',
-                'school_attendance', 'school_evaluation', 'school_event',
-                'school_fees', 'school_hostel', 'school_transport',
-                'timetable'],
+    'depends': ['assignment', 'exam', 'library', 'school_attendance', 
+                'school_evaluation', 'school_event', 'school_fees',
+                'school_hostel', 'school_transport'],
     'installable': True,
     'application': True
 }
