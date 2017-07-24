9230ede30653a9a238c7c08a810a6cb44abe5904
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-07-24 16:31:41 +0530

    [IMP]Solve the travis error.

--- OdooEduErp/__manifest__.py
+++ OdooEduErp/__manifest__.py
@@ -3,16 +3,15 @@
 
 {
     'name': 'Education ERP',
-    'version': '10.0.1.0.1',
-    'author': '''Serpent Consulting Services Pvt. Ltd.''',
+    'version': '10.0.1.0.0',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'category': 'EduERP Management',
     'website': 'http://www.serpentcs.com',
-    'category': 'EduErp',
-    'license': 'AGPL-3',
-    'complexity': 'easy',
-    'summary': 'Install all the modules',
-    'depends': ['assignment', 'exam', 'library', 'school_attendance', 
+    'depends': ['assignment', 'exam', 'library', 'school_attendance',
                 'school_evaluation', 'school_event', 'school_fees',
                 'school_hostel', 'school_transport'],
+    'license': 'AGPL-3',
+    'auto_install': False,
     'installable': True,
     'application': True
 }
