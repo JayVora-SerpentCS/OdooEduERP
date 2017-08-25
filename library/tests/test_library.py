0be14ab4b20ecf9aad5e52ed3801114fe7bc3b5c
arpit.joshi.serpentcs@gmail.com <arpit.joshi.serpentcs@gmail.com>
2017-08-25 16:44:15 +0530

    [IMP]solve test cases

--- library/tests/test_library.py
+++ library/tests/test_library.py
@@ -2,6 +2,8 @@
 
 from odoo.tests import common
 import time
+from datetime import datetime
+from dateutil.relativedelta import relativedelta as rd
 
 
 class TestLibrary(common.TransactionCase):
@@ -20,15 +22,23 @@ class TestLibrary(common.TransactionCase):
         self.library_book_issue_obj = self.env['library.book.issue']
         self.stock_picking_obj = self.env['stock.picking']
         self.immideate_transfer = self.env['stock.immediate.transfer']
-        self.student_id = self.env.ref('school.demo_student_student_5')
-        self.school_standard = self.env.ref('school.demo_school_standard_2')
-        self.standard = self.env.ref('school.demo_standard_standard_2')
+        self.student_id = self.env.ref('school.demo_student_student_9')
+        self.school_standard = self.env.ref('school.demo_school_standard_3')
+        self.standard = self.env.ref('school.demo_standard_standard_3')
         self.product = self.env.ref('library.library_product_b2')
         self.res_partner = self.env.ref('base.res_partner_1')
         self.category = self.env.ref('product.product_category_1')
         self.company_id = self.env.ref('school.demo_school_1')
         self.product_book = self.env.ref('library.product_product_b1')
         self.prd_uom = self.env.ref('product.product_uom_unit')
+        currdt = datetime.now()
+        return_date = currdt + rd(days=4)
+        ret_date = datetime.strftime(return_date, '%Y-%m-%d %H:%M:%S')
+        curent_date = datetime.strftime(currdt, '%Y-%m-%d %H:%M:%S')
+        st_date = currdt - rd(months=4)
+        start_dt = datetime.strftime(st_date, '%m/%d/%Y')
+        end_dt = currdt + rd(months=14)
+        end_date = datetime.strftime(end_dt, '%m/%d/%Y')
         # Create library rack
         self.library_rack = self.library_rack_obj.\
             create({'name': 'Rack34',
@@ -46,11 +56,11 @@ class TestLibrary(common.TransactionCase):
                     })
         # Create product
         categ = self.env['product.category'].search([('name', '=', 'Books')])
-        self.product_product = self.product_product_obj.\
+        self.product_product2 = self.product_product_obj.\
             create({'name': 'Java',
                     'categ_id': categ.id,
                     'type': 'product',
-                    'day_to_return_book': 10,
+                    'day_to_return_book': 3,
                     'weight': 1.23,
                     'fine_lost': 100,
                     'fine_late_return': 100,
@@ -61,32 +71,35 @@ class TestLibrary(common.TransactionCase):
         # Create editior
         self.book_editor = self.book_editor_obj.\
             create({'name': 'S.S Prasad',
-                    'book_id': self.product_product.id
+                    'book_id': self.product_product2.id
                     })
         # Create library card
-        self.library_card = self.library_card_obj.\
-            create({'code': 'C0056',
+        self.library_card2 = self.library_card_obj.\
+            create({'code': 'c4579877687',
                     'user': 'student',
-                    'book_limit': 10,
+                    'book_limit': 20,
                     'student_id': self.student_id.id,
-                    'roll_no': 2,
+                    'roll_no': 7,
                     'standard_id': self.school_standard.id,
+                    'start_date': start_dt,
+                    'end_date': end_date,
                     })
-        self.library_card._compute_name()
+        self.library_card2._compute_name()
+        self.library_card2.running_state()
         # Create purchase order
         self.purchase_order = self.purchase_order_obj.\
             create({'partner_id': self.res_partner.id,
-                    'date_order': time.strftime('06-29-2017 16:58:10'),
-                    'date_planned': time.strftime('06-29-2017 16:58:10'),
+                    'date_order': curent_date,
+                    'date_planned': curent_date,
                     })
         # Create purchase order line
         self.purchase_order_line = self.purchase_order_line_obj.\
-            create({'product_id': self.product_product.id,
-                    'name': 'Java Book',
-                    'date_planned': time.strftime('06-29-2017 16:58:10'),
+            create({'product_id': self.product_product2.id,
+                    'name': 'Java',
+                    'date_planned': curent_date,
                     'company_id': self.company_id.id,
-                    'product_qty': 10.0,
-                    'price_unit': 250,
+                    'product_qty': 100.0,
+                    'price_unit': 2500,
                     'product_uom': self.prd_uom.id,
                     'order_id': self.purchase_order.id
                     })
@@ -104,37 +117,37 @@ class TestLibrary(common.TransactionCase):
         self.library_book_request = self.library_book_request_obj.\
             create({'req_id': 'New',
                     'type': 'existing',
-                    'card_id': self.library_card.id,
-                    'name': self.product_product.id,
-                    'book_return_days': 10
+                    'card_id': self.library_card2.id,
+                    'name': self.product_product2.id,
+                    'book_return_days': 3
                     })
         self.library_book_request._compute_bname()
         self.library_book_request.draft_book_request()
         self.library_book_request.confirm_book_request()
         self.library_book_request.cancle_book_request()
-        # Book issue created
-        self.library_book_issue = self.library_book_issue_obj.\
-            create({'issue_code': 'L097',
-                    'name': self.product_product.id,
-                    'card_id': self.library_card.id,
+
+        self.library_book_issue2 = self.library_book_issue_obj.\
+            create({'issue_code': 'L0976789878',
+                    'name': self.product_product2.id,
+                    'card_id': self.library_card2.id,
                     'user': 'Student',
+                    'state': 'draft',
                     'student_id': self.student_id.id,
                     'standard_id': self.standard.id,
-                    'roll_no': 2,
-                    'actual_return_date': time.strftime('06-29-2017 16:58:10'),
+                    'roll_no': 7,
+                    'actual_return_date': ret_date,
                     })
-        self.library_book_issue.onchange_day_to_return_book()
-        self.library_book_issue._compute_return_date()
-        self.library_book_issue._compute_penalty()
-        self.library_book_issue._compute_lost_penalty()
-        self.library_book_issue._check_issue_book_limit()
-        self.library_book_issue.onchange_card_issue()
-        self.library_book_issue.draft_book()
-        self.library_book_issue.issue_book()
-        self.library_book_issue.reissue_book()
-        self.library_book_issue.return_book()
-        self.library_book_issue.cancel_book()
+        self.library_book_issue2.onchange_day_to_return_book()
+        self.library_book_issue2._compute_return_date()
+        self.library_book_issue2._compute_penalty()
+        self.library_book_issue2._compute_lost_penalty()
+        self.library_book_issue2._check_issue_book_limit()
+        self.library_book_issue2.onchange_card_issue()
+        self.library_book_issue2.issue_book()
+        self.library_book_issue2.reissue_book()
+        self.library_book_issue2.return_book()
+        self.library_book_issue2.cancel_book()
 
     def test_exam(self):
-        self.assertEqual(self.library_card.student_id.state, 'done')
-        self.assertEqual(self.library_book_issue.student_id.state, 'done')
+        self.assertEqual(self.library_card2.student_id.state, 'done')
+        self.assertEqual(self.library_book_issue2.student_id.state, 'done')
