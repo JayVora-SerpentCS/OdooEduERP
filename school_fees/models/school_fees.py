d1da3e220935fde76bc3681714ba2e324a143244
Nisha Gajjar <n.gajjar.serpentcs@gmail.com>
2018-04-18 11:47:12 +0530

    [IMP]improve code to resolve travis warnings

--- school_fees/models/school_fees.py
+++ school_fees/models/school_fees.py
@@ -321,7 +321,6 @@ class StudentPayslip(models.Model):
                 line_vals = {'slip_id': rec.id,
                              'name': data.name,
                              'code': data.code,
-                             'sequence': data.sequence,
                              'type': data.type,
                              'account_id': data.account_id.id,
                              'amount': data.amount,
