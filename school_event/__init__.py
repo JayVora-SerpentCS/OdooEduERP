93f849a1ea308fa54454b30e8f4bbbd000f82e89
Arpit Joshi <arpit.joshi.serpentcs@gmail.com>
2018-05-02 18:55:00 +0530

    [IMP] migrated school_event module in v11

--- school_event/__init__.py
+++ school_event/__init__.py
@@ -2,7 +2,7 @@
 # See LICENSE file for full copyright and licensing details.
 
 # ----------------------------------------------------------
-# A Module to For Management of Events in School
+# A Module for For Management of Events in School
 # ----------------------------------------------------------
 
 from . import models
