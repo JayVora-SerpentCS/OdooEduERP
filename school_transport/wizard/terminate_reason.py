d969cb823defeb3db6d57209088d879365a7c2e3
Arpit Joshi <arpit.joshi.serpentcs@gmail.com>
2018-07-03 19:10:42 +0530

    [IMP]made imp in school transport.Added field to display message if student is registered in transport root while terminating

---
+++ school_transport/wizard/terminate_reason.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+
+from odoo import models, fields, api,_
+from odoo.exceptions import ValidationError
+
+
+class TerminateReasonTransport(models.TransientModel):
+    _inherit = 'terminate.reason'
+
+    transport_info = fields.Text('Transport Info')
+
+    @api.model
+    def default_get(self, fields):
+        res = super(TerminateReasonTransport, self).default_get(fields)
+        student = self._context.get('active_id')
+        student_obj = self.env['student.student'].browse(student)
+        student_transport = self.env['transport.registration'].\
+        search([('part_name', '=', student_obj.id),
+                ('state', 'in', ['confirm', 'pending', 'paid'])])
+        transport_msg = ''
+        if student_transport:
+            for rec in student_transport:
+                transport_msg += '\nStudent is registered for the root' + ' '\
+                    + rec.name.name + ' ' + 'the vehicle number is' + ' ' +\
+                    rec.vehicle_id.vehicle + ' and point number is ' +\
+                    rec.point_id.name
+        res.update({'transport_info': transport_msg})
+        return res
