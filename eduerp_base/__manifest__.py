d383686aa974d4f490a9f5fa18de3fb75e4810ad
Anu Patel <a.patel.serpentcs@gmail.com>
2018-11-21 16:41:01 +0530

    [IMP] Improved the code by removed unused fields.

--- eduerp_base/__manifest__.py
+++ eduerp_base/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Education ERP',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
     'category': 'EduERP Management',
     'website': 'http://www.serpentcs.com',
