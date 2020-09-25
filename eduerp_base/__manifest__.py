0f9067cd9fc158ef4f137cc056467a21c614538e
Foram Darji <foram.d.serpentcs@gmail.com>
2020-09-25 19:38:26 +0530

    [WIP][MIG]eduerp_base: Migration to v14.

--- eduerp_base/__manifest__.py
+++ eduerp_base/__manifest__.py
@@ -1,16 +1,24 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Education ERP',
-    'version': '13.0.1.0.0',
-    'author': 'Serpent Consulting Services Pvt. Ltd.',
-    'category': 'EduERP Management',
-    'website': 'http://www.serpentcs.com',
-    'depends': ['assignment', 'exam', 'library', 'school_attendance',
-                'school_evaluation', 'school_event', 'school_fees',
-                'school_hostel', 'school_transport'],
-    'license': 'AGPL-3',
-    'auto_install': False,
-    'installable': True,
-    'application': True
+    "name": "Education ERP",
+    "version": "14.0.1.0.0",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "category": "EduERP Management",
+    "website": "http://www.serpentcs.com",
+    "depends": [
+        "assignment",
+        "exam",
+        "library",
+        "school_attendance",
+        "school_evaluation",
+        "school_event",
+        "school_fees",
+        "school_hostel",
+        "school_transport",
+    ],
+    "license": "AGPL-3",
+    "auto_install": False,
+    "installable": True,
+    "application": True,
 }
