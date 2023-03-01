2aa3f3fbfd3a3121f4b7b509bdef11f9a919e0f7
Rajan-SCS <rajan.s.serpentcs@gmail.com>
2023-03-01 17:35:21 +0530

    [MIG] school : Migrated in v15.

--- school/models/res_users.py
+++ school/models/res_users.py
@@ -10,14 +10,19 @@ class ResUsers(models.Model):
     @api.model
     def create(self, vals):
         """Inherit Method to create user of group teacher or parent."""
-        vals.update({'employee_ids': False})
+        vals.update({"employee_ids": False})
         res = super(ResUsers, self).create(vals)
-        if self._context.get('teacher_create', False):
+        if self._context.get("teacher_create", False):
             teacher_group_ids = [
-                self.env.ref('school.group_school_teacher').id,
-                self.env.ref('base.group_user').id,
-                self.env.ref('base.group_partner_manager').id]
-            res.write({'groups_id': [(6, 0, teacher_group_ids)],
-                    'company_id': self._context.get('school_id'),
-                    'company_ids': [(4, self._context.get('school_id'))]})
+                self.env.ref("school.group_school_teacher").id,
+                self.env.ref("base.group_user").id,
+                self.env.ref("base.group_partner_manager").id,
+            ]
+            res.write(
+                {
+                    "groups_id": [(6, 0, teacher_group_ids)],
+                    "company_id": self._context.get("school_id"),
+                    "company_ids": [(4, self._context.get("school_id"))],
+                }
+            )
         return res
