9b9351058f811774cdcbca5af677abf8730ec46d
Rajan-SCS <rajan.s.serpentcs@gmail.com>
2023-03-09 19:08:36 +0530

    [MIG] assignment: Migrated in v15.

--- assignment/wizard/reason.py
+++ assignment/wizard/reason.py
@@ -9,15 +9,18 @@ class RejectReason(models.TransientModel):
     _name = "reject.reason"
     _description = "Reason of Rejection Details"
 
-    reasons = fields.Text("Reject Reason",
-        help="Enter assignment reject reason here")
+    reasons = fields.Text(
+        "Reject Reason", help="Enter assignment reject reason here"
+    )
 
     def save_reason(self):
         """Method to write reason in assignment model from wizard"""
         student_assignment = self.env["school.student.assignment"]
         for rec in self:
             if self.env.context.get("active_id"):
-                student_assignment.search([
-                    ('id', '=', self.env.context.get("active_id")), limit]).write(
-                    {"state": "reject", "rejection_reason": rec.reasons or ""})
+                student_assignment.search(
+                    [("id", "=", self.env.context.get("active_id"))], limit=1
+                ).write(
+                    {"state": "reject", "rejection_reason": rec.reasons or ""}
+                )
         return True
