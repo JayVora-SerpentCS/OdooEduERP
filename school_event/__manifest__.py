924f0f00a83c63201786d487afa675eaa12518ca
RiteshSerpentCS <ritesh.b.serpentcs@gmail.com>
2025-11-17 19:27:28 +0530

    [IMP] changed website

--- school_event/__manifest__.py
+++ school_event/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "School Event Management For Education ERP",
     "version": "18.0.1.0.0",
     "author": "Serpent Consulting Services Pvt. Ltd.",
-    "website": "http://www.serpentcs.com",
+    "website": "https://www.serpentcs.com",
     "images": ["static/description/Banner_school_event_17.png"],
     "category": "School Management",
     "license": "AGPL-3",
